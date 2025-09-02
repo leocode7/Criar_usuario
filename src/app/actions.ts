5eebebfce823b5b74227f220b6c2b208f0d7fbae
leonardo <vieiraleo10@hotmail.com>
2025-09-02 18:35:16 -0300

    Subindo projeto inicial

---
+++ src/app/actions.ts
@@ -0,0 +1,14 @@
+'use server'
+
+import db from "../../prisma/db"
+
+// Criar usuário
+export async function createUser(data: {email: string; name: string}) {
+  const user = await db.user.create({data})
+  return user
+}
+
+// Listar usuários
+export async function getUsers() {
+  return await db.user.findMany()
+}
