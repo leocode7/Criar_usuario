3b3dfd09ff0bd2329f8c2cfb08d1a0ab6e8e6524
leonardo <vieiraleo10@hotmail.com>
2025-09-02 23:29:40 -0300

    inclusão do `revalidatePath` e `revalidate` para forçar o recarregamento da home

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,12 +2,14 @@
 
 import { revalidatePath } from "next/cache";
 import db from "../../prisma/db"
+import { redirect } from "next/navigation";
 
 // Criar usuário
 export async function createUser(data: {email: string; name: string}) {
   const user = await db.user.create({data})
   return user
   revalidatePath('/')
+  redirect('/')
 }
 
 // Listar usuários
